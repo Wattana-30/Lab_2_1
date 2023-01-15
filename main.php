8616f897360eafcea814bcad25bc854686ec1dfe
WattanaTH <wattana.po@ku.th>
2023-01-15 15:53:15 +0700

    commit

---
+++ main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1>แบบสอบถาม</h1>
+
+    <form action="show.php" method="get" style="margin-left: 80px;">
+    ชื่อ:<input type="text" name="name"><br>
+        การุณาระบุเพศ:
+        <select name="gender">
+            <option value="male">ชาย</option>
+            <option value="female">หญิง</option>
+        </select><br>
+        การุณาใส่อายุ:
+        <input type="number" name="num" style="width: 40px;"><br>
+        สถานะ:
+        <select name="status">
+            <option value="single">โสด</option>
+            <option value="marry">แต่งงานแล้ว</option>
+        </select><br>
+        <input type="submit" value="ส่ง">
+
+
+    </form>
+
+</body>
+
+</html>
